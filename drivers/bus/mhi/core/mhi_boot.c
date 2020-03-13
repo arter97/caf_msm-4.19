8da9177e8d1b5c60d2a03851127ad191f3f12fd2
Jeffrey Hugo <jhugo@codeaurora.org>
2020-03-13 08:13:47 -0600

    mhi_bus: core: Add missing includes
    When porting the code to other kernel versions, some includes where found
    to be missing.  Add them.
    
    Change-Id: I4b16e027aa219284450573abd83ab0dcd4a7db8a
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>
    Signed-off-by: Pratyush Anand <pratanan@codeaurora.org>

--- drivers/bus/mhi/core/mhi_boot.c
+++ drivers/bus/mhi/core/mhi_boot.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-/* Copyright (c) 2018-2019, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2018-2020, The Linux Foundation. All rights reserved. */
 
 #include <linux/debugfs.h>
 #include <linux/delay.h>
@@ -12,6 +12,7 @@
 #include <linux/of.h>
 #include <linux/module.h>
 #include <linux/random.h>
+#include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/wait.h>
 #include <linux/mhi.h>
