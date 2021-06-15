c3d0a8cf1f4d4698f66827937ad0f88e854dd617
Hareesh Gundu <hareeshg@codeaurora.org>
2021-06-15 10:53:15 +0530

    msm: kgsl: Keep private intact until last refcount is put
    Currently, we are partially freeing private once last file is
    closed but closing last file doesn't necessarily means that
    all refcount on private is also put. This results in having a
    partially initialized private struct which is not yet freed
    and also can't be re-used. As a result if an application is
    just opening and closing kgsl device in a loop we will end up
    allocating and initializing a new private every time. This also
    allows application to mis-use this and create more than allowed
    limit of 200 contexts as they can use newly created private to
    create new context every time.
    
    To fix this, keep private intact until last refcount is put.
    This ensures we can re-use same private if same pid is doing
    kgsl open and close repeatedly and also ensures that an app.
    can't breach 200 context limit.
    
    Change-Id: Ia1d1071275c7691a3d50bc15477b71387c5806d6
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>
    Signed-off-by: Hareesh Gundu <hareeshg@codeaurora.org>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -246,13 +246,9 @@ static ssize_t process_sysfs_store(struct kobject *kobj,
 	return -EIO;
 }
 
+/* Dummy release function - we have nothing to do here */
 static void process_sysfs_release(struct kobject *kobj)
 {
-	struct kgsl_process_private *priv;
-
-	priv = container_of(kobj, struct kgsl_process_private, kobj);
-	/* Put the refcount we got in kgsl_process_init_sysfs */
-	kgsl_process_private_put(priv);
 }
 
 static const struct sysfs_ops process_sysfs_ops = {
@@ -299,9 +295,6 @@ void kgsl_process_init_sysfs(struct kgsl_device *device,
 		struct kgsl_process_private *private)
 {
 	int i;
-
-	/* Keep private valid until the sysfs enries are removed. */
-	kgsl_process_private_get(private);
 
 	if (kobject_init_and_add(&private->kobj, &process_ktype,
 		kgsl_driver.prockobj, "%d", pid_nr(private->pid))) {
