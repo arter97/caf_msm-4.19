be6b62b910b1c336b33f27925012fa80159f38af
Suren Baghdasaryan <surenb@google.com>
2022-11-21 12:15:43 -0800

    ANDROID: mm/khugepaged: add missing vm_write_{begin|end}
    Speculative page fault handler needs to detect concurrent pmd changes
    and relies on vma seqcount for that. pmdp_collapse_flush(), set_huge_pmd() and collapse_and_free_pmd() can modify a pmd.
    vm_write_{begin|end} are needed in the paths which can call these
    functions for page fault handler to detect pmd changes.
    
    Bug: 257443051
    Change-Id: Ieb784b5f44901b66a594f61b9e7c91190ff97f80
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Git-commit: 5ed391bd8ad8481d82c1bbb05a35f5538966dce9
    Git-repo: https://android.googlesource.com/kernel/common/
    [quic_c_spathi@quicinc.com: resolve trivial merge conflicts]
    Signed-off-by: Srinivasarao Pathipati <quic_c_spathi@quicinc.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1279,12 +1279,14 @@ static void retract_page_tables(struct address_space *mapping, pgoff_t pgoff)
 		 * the system too much.
 		 */
 		if (down_write_trylock(&vma->vm_mm->mmap_sem)) {
+			vm_write_begin(vma);
 			spinlock_t *ptl = pmd_lock(vma->vm_mm, pmd);
 			/* assume page table is clear */
 			_pmd = pmdp_collapse_flush(vma, addr, pmd);
 			spin_unlock(ptl);
 			up_write(&vma->vm_mm->mmap_sem);
 			mm_dec_nr_ptes(vma->vm_mm);
+			vm_write_end(vma);
 			pte_free(vma->vm_mm, pmd_pgtable(_pmd));
 		}
 	}
